08d169bb62bc884bab4972be68aa050a1e44d63e
Brad Nolan <bnolan56@gmail.com>
2018-08-26 13:30:30 -0600

    trying to debug why connection times out when visiting page

--- server.js
+++ server.js
@@ -24,5 +24,5 @@ app.use('/', routes);
 
 // Starts the server to begin listening
 // =============================================================
-const PORT = process.env.PORT || 3306;
-app.listen(`Application Listening on + ${PORT}`);
+const PORT = process.env.PORT || 3000;
+console.log("Application Listening on " + PORT);
