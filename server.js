7f789eba0f520c214d6e38a0e187db13798c6444
Brad Nolan <bnolan56@gmail.com>
2018-08-27 18:46:43 -0600

    removing old code

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 // =============================================================
 const express = require('express');
 const bodyParser = require('body-parser');
-const methodOverride = require('method-override');
 
 const app = express();
 
